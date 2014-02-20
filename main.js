a173eca9765350aebb4fb30734d2cff38cffe62f
Michael Herman <hermanmu@gmail.com>
2014-02-19 19:15:58 -0700

    updates

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  $('#meetup').click(function(event){
+    getMeetups($('#topic').val(), $('#zipcode').val())
+  });
+});
+
+function getMeetups(topic, zipcode){
+	var api_key = "50722e1d56c194e61763a2ee1e4535";
+	var url = "https://api.meetup.com/2/";
+	var method = "open_events"
+	$.ajax({
+		url: url + method,
+		data: {key: api_key, zip: zipcode,topic: topic},
+		crossDomain: true,
+		dataType: 'jsonp',
+		type: "GET",
+		success: function (data) {
+		  parseMeetups(data.results)
+		},
+		error: function(data) {
+			console.log("Error", data);
+		}				
+	});
+}
+
+function parseMeetups(results){
+  for(var i = 0; i < results.length; i ++){
+    var div = $('<div class ="event"></div>');
+    var name = $('<div> Name: '+ results[i].name+'</div>');
+    var description = $('<div> Description: '+ results[i].description+'</div>');
+    var group = $('<div> Group: '+ results[i].group.name+'</div>');
+    var link = $('<div> Learn More: <a href ="'+results[i].event_url+'" target = "_blank">'+results[i].event_url+'</a></div>')
+    div.append(name, description, group, link);
+    $('#events').append(div);
+  } 
+}
