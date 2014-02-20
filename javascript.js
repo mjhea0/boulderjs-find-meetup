99896552955106578253b52a6c8f7e3d3c42e1d8
Michael Herman <hermanmu@gmail.com>
2014-02-20 00:10:06 -0700

    updates

--- javascript.js
+++ javascript.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
-  $('#meetup').submit(function(event){
+  $('#meetups').submit(function(event){
     getMeetups($('#topic').val(), $('#zipcode').val())
     return false;
   });
 });
 
 function getMeetups(topic, zipcode){
-	var api_key = "get_your_own";
-	var url = "https://api.meetup.com/2/";
-	var method = "open_events"
-	$.ajax({
-		url: url + method,
-		data: {key: api_key, zip: zipcode,topic: topic},
-		crossDomain: true,
-		dataType: 'jsonp',
-		type: "GET",
-		success: function (data) {
-		  parseMeetups(data.results)
-		},
-		error: function(data) {
-			console.log("Error", data);
-		}				
-	});
+  var api_key = "get_your_own";
+  var url = "https://api.meetup.com/2/";
+  var method = "open_events"
+  $.ajax({
+    url: url + method,
+    data: {key: api_key, zip: zipcode,topic: topic},
+    crossDomain: true,
+    dataType: 'jsonp',
+    type: "GET",
+    success: function (data) {
+      parseMeetups(data.results)
+    },
+    error: function(data) {
+      console.log("Error", data);
+    }
+  });
 }
 
 function parseMeetups(results){
   for(var i = 0; i < results.length; i ++){
-    var div = $('<div class ="event"></div>');
+    var div = $('<div></div>');
     var name = $('<div> Name: '+ results[i].name+'</div>');
     var description = $('<div> Description: '+ results[i].description+'</div>');
     var group = $('<div> Group: '+ results[i].group.name+'</div>');
     var link = $('<div> Learn More: <a href ="'+results[i].event_url+'" target = "_blank">'+results[i].event_url+'</a></div>')
     div.append(name, description, group, link);
     $('#events').append(div);
-  } 
+  }
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+## To do / Stretch Goals
+
+1. Add comments to code
+2. Add reset button - remove events, clear inputs
+3. Add data to tables
+3. Add mouse enter / mouse leave to events
+4. Add seperate config file for Meetup info
